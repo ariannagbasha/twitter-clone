f4970dfc15adc53db08c6f8f7ca12a09a70671ef
Arianna Basha <ariannagbasha@gmail.com>
2020-06-10 11:01:06 -0500

    Merged login-forms

---
+++ src/react/components/UserAccount.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Spinner from "react-spinkit";
+
+import "./UserAccount.css";
+
+class userAccount extends React.Component {
+  state = { displayName: "" , about: "" };
+
+
+
+  render() {
+  
+    return (
+      <React.Fragment>
+          Awesome
+    
+      </React.Fragment>
+    );
+  }
+}
+
+export default connect(
+  state => ({
+    result: state.users.register.result,
+    loading: state.users.register.loading,
+    error: state.users.register.error
+  }),
+  { register, getMessages }
+)(RegisterForm);
---
+++ src/redux/usersAccount.js
@@ -0,0 +1,35 @@
+import {
+    domain,
+    jsonHeaders,
+    handleJsonResponse,
+    getInitStateFromStorage,
+    asyncInitialState,
+    asyncCases,
+    createActions,
+    createReducer
+  } from "./helpers";
+
+  const url = domain + `/profiles/${username}`; 
+
+
+  const USER_INFO = createActions("getUserInfo");
+  export const user = userInfo => dispatch => {
+    dispatch(USER_INFO.START());
+  
+    return fetch(url, {
+      method: "GET",
+      headers: jsonHeaders,
+      body: JSON.stringify(getUserInfo)
+    })
+      .then(handleJsonResponse)
+      .then(result => dispatch(USER_INFO.SUCCESS(result))) // Result will be the object that I see on the Swagger docs under responses section
+      .catch(err => Promise.reject(dispatch(USER_INFO.FAIL(err))));
+  };
+
+  export const reducers = {
+    register: createReducer(getInitStateFromStorage("getUserInfo", asyncInitialState), {
+      ...asyncCases(USER_INFO),
+      [USER_INFO.SUCCESS.toString()]: (state, action) => asyncInitialState
+    })
+ 
+  };
